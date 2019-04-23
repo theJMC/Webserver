f864e067df387bad2ac200df54bc1aacdc6ceef4
_TheJMC_ <legojames99@gmail.com>
2019-04-23 11:14:56 +0100

    Added a Target ID Field

--- Hue-Control/app.js
+++ Hue-Control/app.js
@@ -21,18 +21,21 @@ $.getJSON(url_path + "/lights", function(data){
     console.log(light_names)
 
     off.onclick = function(light){
-        console.log("Turned Light Off");
-        xhttp.open("PUT", url_path + "/lights/5/state", true);
+        var light_id = document.getElementById("id_selection").value;
+        console.log("Turned " + light_names[light_id] + " Off");
+        xhttp.open("PUT", url_path + "/lights/" + light_id + "/state", true);
         xhttp.send('{"on": false}');
     };
 
     on.onclick = function(light){
-        console.log("Turned Light On");
-        xhttp.open("PUT", url_path + "/lights/5/state", true);
+        var light_id = document.getElementById("id_selection").value;
+        console.log("Turned " + light_names[light_id] + " On");
+        xhttp.open("PUT", url_path + "/lights/" + light_id + "/state", true);
         xhttp.send('{"on": true}');
     };
 
     reset.onclick = function(light){
+        var light_id = document.getElementById("id_selection");
         console.log("Turned on Colour Loop")
         xhttp.open("PUT", url_path + "/lights/5/state", true);
         xhttp.send('{"bri":254, "ct":153}')
