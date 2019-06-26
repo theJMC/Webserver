aba77009ed51f008fe5ce30acb4a848a81b07981
_TheJMC_ <legojames99@gmail.com>
2019-06-26 16:04:56 +0100

    added many things

--- Image-Server/upload.php
+++ Image-Server/upload.php
@@ -1,7 +1,8 @@
 <?php
-if (isset($_POST['submit'])){
+if (isset($_POST['file'])) {
+#if (!empty($_POST)) {
     $file = $_FILES['file'];
-    $FileSaveName = $_POST['nameOfFile']
+    #$FileSaveName = $_POST['nameOfFile'];
 
     $fileName = $file['name'];
     $fileTmpName = $file['tmp_name'];
@@ -12,23 +13,33 @@ if (isset($_POST['submit'])){
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
 
-    $allowed = array('jpg', 'jpeg', 'png', 'nef');
-
-    if (in_array($fileActualExt, $allowed)) {
-        if ($fileError === 0){
+    #$allowed = array('.jpg', '.jpeg', '.png', '.nef');
+    #if (in_array($fileActualExt, $allowed)) {
+        if ($fileError !== 1){
             if ($fileSize < 50000) {
-                $fileNameNew = $FileSaveName .".".$fileActualExt;
+                # $fileNameNew = $FileSaveName .".".$fileActualExt;
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
                 $fileDestination = "uploads/".$fileNameNew;
                 move_uploaded_file($fileTmpName, $fileDestination);
                 header("Location: index.html?uploadsuccess");
+                echo 'Success';
             } else {
                 echo "Your file is too big!";
             }
         } else {
             echo "There was an error uploading your file";
+            #echo $fileError;
+            #echo $fileType;
+            echo $file;
+
         }
-    } else {
+    /*} else {
         echo "You cannot upload files of this type!";
-    }
-} 
+        echo 'File Type: '.$fileActualExt;
+        echo 'File Extension: '.end($fileExt);
+        print_r($file);
+    }*/
+} else {
+    echo 'Error 404';
+};
 ?>
